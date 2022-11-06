cb56a562153c291d2a9e4fd676166855246f9a5b
just like before <agangdi@gmail.com>
2022-11-06 14:06:28 +0800

    mint with nonreentrant

--- erc-20/abc.sol
+++ erc-20/abc.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract ABCToken is ERC20, Ownable {
+contract SBCToken is ERC20, Ownable, ReentrancyGuard {
 
-    constructor() ERC20("ABC TOKEN", "ABC") {}
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+    }
 
-    function mint(address account, uint256 amount) external onlyOwner {
+    function mint(address account, uint256 amount) external onlyOwner nonReentrant {
         _mint(account, amount);
     }
 
